9465cf0aeb444600718067f10acb79440b6b2243
lunarmint <me@chocomint.dev>
2026-06-12 15:19:58 -0500

    refactor: store iso 639-1 directly without middleman dict

--- utils/languages.py
+++ utils/languages.py
@@ -6,45 +6,24 @@ AUDIO_LANGUAGES = {
 }
 
 SUBTITLES_LANGUAGES = {
-    "CHS": ("chi-CN", "简体中文"),
-    "CHT": ("chi-TW", "繁體中文"),
-    "DE": ("ger", "Deutsch"),
-    "EN": ("eng", "English"),
-    "ES": ("spa", "Español"),
-    "FR": ("fre", "Français"),
-    "ID": ("ind", "Bahasa Indonesia"),
-    "IT": ("ita", "Italiano"),
-    "JP": ("jpn", "日本語"),
-    "KR": ("kor", "한국어"),
-    "PT": ("por", "Português"),
-    "RU": ("rus", "Русский"),
-    "TH": ("tha", "ภาษาไทย"),
-    "TR": ("tur", "Türkçe"),
-    "VI": ("vie", "Tiếng Việt"),
-}
-
-DICTIONARY = {
-    "chi": "zh",
-    "ger": "de",
-    "eng": "en",
-    "spa": "es",
-    "fre": "fr",
-    "ind": "id",
-    "ita": "it",
-    "jpn": "ja",
-    "kor": "ko",
-    "por": "pt",
-    "rus": "ru",
-    "tha": "th",
-    "tur": "tr",
-    "vie": "vi",
-    "und": "und",
-    "chi-CN": "zh",
-    "chi-TW": "zh",
+    "CHS": ("zh", "简体中文"),
+    "CHT": ("zh", "繁體中文"),
+    "DE": ("de", "Deutsch"),
+    "EN": ("en", "English"),
+    "ES": ("es", "Español"),
+    "FR": ("fr", "Français"),
+    "ID": ("id", "Bahasa Indonesia"),
+    "IT": ("it", "Italiano"),
+    "JP": ("ja", "日本語"),
+    "KR": ("ko", "한국어"),
+    "PT": ("pt", "Português"),
+    "RU": ("ru", "Русский"),
+    "TH": ("th", "ภาษาไทย"),
+    "TR": ("tr", "Türkçe"),
+    "VI": ("vi", "Tiếng Việt"),
 }
 
 
 def get_language(lang: str) -> str:
     """Translate subtitles language code from file name to ISO 639-1."""
-    code, lang = SUBTITLES_LANGUAGES.get(lang, ("und", "Unknown"))
-    return DICTIONARY.get(code, "und")
+    return SUBTITLES_LANGUAGES.get(lang, ("und", "Unknown"))[0]
